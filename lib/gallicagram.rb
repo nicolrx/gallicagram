8774d68353454699931f2ff1215bf71897587a5b
nico_lrx <nicolas.leroux@sciencespo.fr>
2023-05-29 22:33:27 +0300

    resolution update

--- lib/gallicagram.rb
+++ lib/gallicagram.rb
@@ -6,25 +6,27 @@ class Gallicagram
   
 	# query = string or array of strings
 	# corpus = ["lemonde","livres","presse"]
-	# resolution = ["year", "month"]
-	def self.search(query,corpus="lemonde",start_date="1900",end_date="2000",resolution="month",sum=false)
+	# resolution = ["mois", "annee"]
+	def self.search(query,corpus="lemonde",start_date="1900",end_date="2000",resolution="annee",sum=false)
 
 		query = [query] unless query.kind_of?(Array)
-		output = ""
+		data = ""
 		query.each_with_index do |word, index|
 			query = format_query(word, sum)
-			response = call_api(word, corpus, start_date, end_date)
+			response = call_api(word, corpus, start_date, end_date,resolution)
 			unless index == 0
 				response = response.gsub("n,gram,annee,mois,jour,total", "\n").strip
 			end
-			output << response
+			data << response
 		end
 
-		if corpus == "livres" && resolution == "month"
-			resolution = "year"
+		if corpus == "livres" && resolution == "mois"
+			resolution = "annee"
 		end
 
-		data = group_by_resolution(output, resolution)
+		if resolution == "mois"
+			data = group_by_resolution(data)
+		end
 
 		return data
 	end
@@ -40,33 +42,24 @@ def format_query(query, sum)
 
 end
 
-def call_api(query, corpus, start_date, end_date)
+def call_api(query, corpus, start_date, end_date, resolution)
 	# We search through the Gallicagram API
-	url = "https://shiny.ens-paris-saclay.fr/guni/query?corpus=#{corpus}&mot=#{CGI.escape(query)}&from=#{start_date}&to=#{end_date}"
+	url = "https://shiny.ens-paris-saclay.fr/guni/query?corpus=#{corpus}&mot=#{CGI.escape(query)}&from=#{start_date}&to=#{end_date}&resolution=#{resolution}"
 
 	response = URI.open(url)
 
 	return response.read
 end
 
-def group_by_resolution(data, resolution)
+def group_by_resolution(data)
 	csv_parsing = CSV.parse(data, :headers => true)
 	clean_data_array = []
-	case resolution 
-	when "year"
-		csv_parsing.group_by { |word| [word["annee"], word["gram"]] }.each do |cp|
-			sum = cp[1].sum { |word| word["total"].to_i }
-			new_row = cp[0]
-			new_row << sum
-			clean_data_array << new_row
-		end
-	when "month"
-		csv_parsing.group_by { |word| [word["annee"], word["mois"], word["gram"]] }.each do |cp|
-			sum = cp[1].sum { |word| word["total"].to_i }
-			new_row = cp[0]
-			new_row << sum
-			clean_data_array << new_row
-		end
+
+	csv_parsing.group_by { |word| [word["annee"], word["mois"], word["gram"]] }.each do |cp|
+		sum = cp[1].sum { |word| word["total"].to_i }
+		new_row = cp[0]
+		new_row << sum
+		clean_data_array << new_row
 	end
 
 	return clean_data_array
